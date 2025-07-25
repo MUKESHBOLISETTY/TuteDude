65593a8e6c31ed2f008341de14acbd35d2d41e03
Mukesh Bolisetty <mukeshbolisetty09@gmail.com>
2025-07-25 21:37:00 +0530

    Initial code

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from "cookie-parser";
+// import { connect } from './config/database.js';
+// import { cloudinaryConnect } from './config/cloudinary.js';
+import fileUpload from 'express-fileupload';
+
+const app = express();
+const port = 4000;
+
+connect();
+
+app.use(express.json());
+app.use(cookieParser());
+const corsoptions = {
+    origin: "*",
+    methods: "GET, POST, PUT, DELETE, HEAD, PATCH",
+    credentials: true,
+}
+app.use(cors(corsoptions));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp",
+    limits: { fileSize: 50 * 1024 * 1024 },
+  })
+);
+
+// cloudinaryConnect();
+
+app.get('/', (req, res) => {
+    return res.json({
+        success: true,
+        message: "Your server is up and running",
+    });
+});
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}.`);
+});
