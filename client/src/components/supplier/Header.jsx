6dcd0ba799755addc178163f96923fe0cd2bf5b2
Mukesh Bolisetty <mukeshbolisetty09@gmail.com>
2025-07-26 21:58:12 +0530

    removed some unwanted functions

--- client/src/components/supplier/Header.jsx
+++ client/src/components/supplier/Header.jsx
@@ -20,27 +20,7 @@ const Header = ({ title }) => {
         </div>
 
         <div className="flex items-center space-x-4">
-          {/* Search Bar */}
-          <div className="relative">
-            <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
-            <input
-              type="text"
-              placeholder="Search products, orders, vendors..."
-              className="pl-10 pr-4 py-2 w-80 border border-gray-200 rounded-lg focus:ring-2 focus:ring-green-500 focus:border-transparent"
-            />
-          </div>
-
-          {/* Notifications */}
-          <button className="relative p-2 text-gray-400 hover:text-gray-600 hover:bg-gray-100 rounded-lg">
-            <Bell className="w-5 h-5" />
-            <span className="absolute top-0 right-0 w-2 h-2 bg-red-500 rounded-full"></span>
-          </button>
-
-          {/* Settings */}
-          <button className="p-2 text-gray-400 hover:text-gray-600 hover:bg-gray-100 rounded-lg">
-            <Settings className="w-5 h-5" />
-          </button>
-
+         
           {/* Date */}
           <div className="text-right">
             <p className="text-sm font-medium text-gray-900">
